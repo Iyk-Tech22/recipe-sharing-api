f13febf9e850fbaebb1e942dd6b005f401af7aa4
Iyk-Tech <ikechukwugodwin22@yahoo.com>
2025-06-16 14:47:05 +0100

    feat: App config setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const compression = require('compression');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+app.use(helmet());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+app.use(mongoSanitize());
+app.use(xss());
+app.use(compression());
